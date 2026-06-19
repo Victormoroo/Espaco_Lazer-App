6217d250df3a9d098d76bd74e6d5f82f47fdecba
Victormoroo <victorhugo0609@outlook.com>
2026-06-19 12:17:35 -0300

    build: adiciona dependências de navegação com Drawer

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // Obrigatório para o react-native-reanimated 4 (worklets). Deve ser o último plugin.
+    plugins: ['react-native-worklets/plugin'],
   };
 };
